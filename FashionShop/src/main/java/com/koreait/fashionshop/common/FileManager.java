feb0202b0ff8cb675b22038b8bc313bc17aeb5c3
DESKTOP-9O9ULUN\sb031 <sb03173@gmail.com>
2021-01-04 19:27:05 +0900

    2020-01-04

--- FashionShop/src/main/java/com/koreait/fashionshop/common/FileManager.java
+++ FashionShop/src/main/java/com/koreait/fashionshop/common/FileManager.java
@@ -4,15 +4,20 @@
 package com.koreait.fashionshop.common;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+@Data
+@Component /*component-scan의 대상 중 하나임..*/
 public class FileManager {
-	private String saveDir;
-	public void setSaveDir(String saveDir) {
-		this.saveDir = saveDir;
-	}
-	public String getSaveDir() {
-		return saveDir;
-	}
+	private static final Logger logger = LoggerFactory.getLogger(FileManager.class);
+	private String saveBasicDir="/resources/data/basic";
+	private String saveAddonDir="/resources/data/addon";
 	
 	//확장자만 추출하기 
 	public static String getExtend(String path) {
@@ -28,14 +33,18 @@ public class FileManager {
 		return file.delete();
 	}
 	
-	//미리 단위 테스트 해보기 위함..
-	/*
-	public static void main(String[] args) {
-		//   d:\photo\summer\2010\지난.여름에.놀러.갔던.사진.jpg
-		String filename="d:\\photo\\summer\\2010\\지난.여름에.놀러.갔던.사진.jpg";
-		getExtend(filename);
+	//파일 저장하기
+	public void saveFile(String realDir , MultipartFile multi) {
+		try {
+			multi.transferTo(new File(realDir));
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	*/
 }
 
 
--- FashionShop/src/main/java/com/koreait/fashionshop/model/product/service/ProductService.java
+++ FashionShop/src/main/java/com/koreait/fashionshop/model/product/service/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.koreait.fashionshop.common.FileManager;
 import com.koreait.fashionshop.model.domain.Product;
 
 @Service
@@ -11,7 +12,7 @@ public interface ProductService {
 	public List selectAll(); //모든 상품
 	public List selectById(int subcategory_id); //하위 카테고리에 소속된 모든 상품
 	public Product select(int product_id);
-	public void regist(Product product); //insert 하는게 아니기 때문..
+	public void regist(FileManager fileManager, Product product); //insert 하는게 아니기 때문..
 	public void update(Product product);
 	public void delete(int product_id);
 }
